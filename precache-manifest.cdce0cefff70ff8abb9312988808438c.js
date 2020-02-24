b386bb070677d2fc70ff2330cd69356ffa50a2ed
Ciprien Bossut <chefchino@live.com>
2020-02-24 18:46:19 -0500

    Updates

---
+++ precache-manifest.cdce0cefff70ff8abb9312988808438c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cac85c2f5e4026491910ec818d5c876",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "d08bd46f4fdb5872e521",
+    "url": "/clickyGame/static/css/main.0aa99be6.chunk.css"
+  },
+  {
+    "revision": "72a4490dfb8e9609a67e",
+    "url": "/clickyGame/static/js/2.747bbdea.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clickyGame/static/js/2.747bbdea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d08bd46f4fdb5872e521",
+    "url": "/clickyGame/static/js/main.08968b2a.chunk.js"
+  },
+  {
+    "revision": "e6ef4722a3ec8a19f149",
+    "url": "/clickyGame/static/js/runtime-main.d6c7c3c6.js"
+  }
+]);
